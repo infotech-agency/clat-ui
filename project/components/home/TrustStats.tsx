e0cd9fd725266de00d0d657e4775ed83df74d6a8
infotech <infoconwayy@gmail.com>
2026-08-25 18:42:20 +0530

    fac

--- project/components/home/TrustStats.tsx
+++ project/components/home/TrustStats.tsx
@@ -3,7 +3,7 @@ import { StatCounter } from '@/components/shared/StatCounter';
 const STATS = [
   { value: 3, suffix: '', label: 'Centres in Delhi NCR' },
   { value: 5000, suffix: '+', label: 'Students Mentored' },
-  { value: 12, suffix: '+', label: 'Expert Advocate Faculty' },
+  { value: 3, suffix: '+', label: 'Expert Advocate Faculty' },
   { value: 1, suffix: '-on-1', label: 'Personal Mentorship' },
 ];
 
